73255ddca18b4de8df9b768cc7d5c65a6c3e470a
binhan.tran <binhan.tran@okg.com>
2025-04-04 17:01:23 +0800

    Add sample swift test for ObjC code

--- templated_tests/swift_counter/slather.sh
+++ templated_tests/swift_counter/slather.sh
@@ -5,12 +5,14 @@ set -e
 rm -rf ./.report
 rm -rf ./.derivedData
 
+echo "Running tests ..."
+
 xcodebuild clean test \
     -project swift_counter.xcodeproj \
     -scheme swift_counter \
     -destination 'platform=iOS Simulator,name=iPhone 16 Pro' \
     -enableCodeCoverage YES \
-    -derivedDataPath ./.derivedData
+    -derivedDataPath ./.derivedData > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
     echo "xcodebuild failed. Exiting."
---
+++ templated_tests/swift_counter/swift_counterTests/OjbcViewModelSwiftTests.swift
@@ -0,0 +1,14 @@
+//
+//  OjbcViewModelTests.swift
+//  swift_counterTests
+//
+//  Created by Tran Binh An on 4/4/25.
+//
+
+import Foundation
+import XCTest
+@testable import swift_counter
+
+class OjbcViewModelSwiftTests: XCTestCase {
+    func testExample() {}
+}
