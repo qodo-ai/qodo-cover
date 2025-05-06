89f6e67f5e280371ce755861defe84153fa920aa
Andrii Simakov <andsimakov@gmail.com>
2025-05-06 19:23:25 +0300

    Remove get log_level from env var

--- cover_agent/CustomLogger.py
+++ cover_agent/CustomLogger.py
@@ -1,43 +1,37 @@
 import logging
-import os
 
 
 class CustomLogger:
-    VALID_LOG_LEVELS = {
-        "DEBUG": logging.DEBUG,
-        "INFO": logging.INFO,
-        "WARNING": logging.WARNING,
-        "ERROR": logging.ERROR,
-        "CRITICAL": logging.CRITICAL,
-    }
 
     @classmethod
-    def get_logger(cls, name, generate_logs=True):
+    def get_logger(cls, name, generate_logs=True, file_level=logging.INFO, console_level=logging.INFO):
         """
         Return a logger object with specified name.
 
         Parameters:
             name (str): The name of the logger.
             generate_logs (bool): Whether to generate log files.
+            file_level (int): The log level to use.
+            console_level (int): The log level to use.
 
         Returns:
             logging.Logger: The logger object.
 
-        Raises:
-            ValueError: If LOG_LEVEL environment variable contains invalid value.
+        Note:
+            This method sets up the logger to handle all messages of DEBUG level and above.
+            It adds a file handler to write log messages to a file specified by 'log_file_path' and a stream handler
+            to output log messages to the console. The log file is overwritten on each run.
+
+        Example:
+            logger = CustomLogger.get_logger('my_logger')
+            logger.debug('This is a debug message')
+            logger.info('This is an info message')
+            logger.warning('This is a warning message')
+            logger.error('This is an error message')
+            logger.critical('This is a critical message')
         """
         logger = logging.getLogger(name)
-
-        # Get LOG_LEVEL from environment, default to empty string if not set
-        env_log_level = os.environ.get("LOG_LEVEL", "").upper()
-
-        # Check if provided log level is valid when it's not empty
-        if env_log_level and env_log_level not in cls.VALID_LOG_LEVELS:
-            valid_levels = ", ".join(cls.VALID_LOG_LEVELS.keys())
-            raise ValueError(f"Invalid LOG_LEVEL: {env_log_level}. Valid levels are: {valid_levels}")
-
-        log_level = cls.VALID_LOG_LEVELS.get(env_log_level, logging.DEBUG)
-        logger.setLevel(log_level)
+        logger.setLevel(logging.DEBUG)
 
         # Specify the log file path
         log_file_path = "run.log"
@@ -50,13 +44,13 @@ class CustomLogger:
             if generate_logs:
                 # File handler for writing to a file. Use 'w' to overwrite the log file on each run
                 file_handler = logging.FileHandler(log_file_path, mode="w")
-                file_handler.setLevel(logging.INFO)
+                file_handler.setLevel(file_level)
                 file_handler.setFormatter(formatter)
                 logger.addHandler(file_handler)
 
             # Stream handler for output to the console
             stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(log_level)
+            stream_handler.setLevel(console_level)
             stream_handler.setFormatter(formatter)
             logger.addHandler(stream_handler)
 
